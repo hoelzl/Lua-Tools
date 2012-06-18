e7a2d3ee4add20f904a4549f8c43df1ba4392075
Thomas Gabor <github@das.ist.denkfrei.de>
2012-06-18 21:04:59 +0200

    completed modularization

---
+++ src/serialize.lua
@@ -0,0 +1,35 @@
+-- this script is used by distributed.lua
+-- usage ---------------------------------------------------------------------------------
+-- provides a simple serializer                                                         --
+-- EXPORTS: serialize, command                                                          --
+------------------------------------------------------------------------------------------
+
+function serialize(expr, saved, prologue, index)
+	saved = saved or {}
+	prologue = prologue or ""
+	index = index or 1
+	if type(expr) == "number" then
+		return tostring(expr), saved, prologue, index
+	elseif type(expr) == "string" then
+		return string.format("%q", expr), saved, prologue, index
+	elseif type(expr) == "table" then
+		if not saved[expr] then
+			saved[expr] = "x"..tostring(index)
+			prologue = prologue..string.format("local %s={}; ", saved[expr])
+			index = index + 1
+			for key,val in pairs(expr) do
+				kexpr, saved, prologue, index = serialize(key, saved, prologue, index)
+				vexpr, saved, prologue, index = serialize(val, saved, prologue, index)
+				prologue = prologue..string.format("%s[%s]=%s; ", saved[expr], kexpr, vexpr)
+			end
+		end
+		return saved[expr], saved, prologue, index
+	else
+		error("cannot serialize "..type(expr))
+	end
+end
+
+function command(name, param)
+	local object, library, prologue = serialize(param)
+	return prologue.."return "..name.."("..object..")"
+end
